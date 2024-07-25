aa9599ee9f0fd1d6575496814efb1049296ce432
330sum <330sum@naver.com>
2024-07-25 23:36:30 +0900

    Feat(.footer): add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="main-header-container">
+  <div class="wrap">
     <!-- start: header -->
     <header class="main-header">
       <h1>
-        <a href="/" class="main-header__connect"> Connect </a>
+        <a href="/" class="main-header__domain"> COP<span>i</span> </a>
       </h1>
       <nav class="main-nav">
         <ul class="main-nav__items">
           <li class="main-nav__item">
-            <a href="/project" class="nav-link">Project</a>
+            <a href="/project">PROJECT</a>
           </li>
           <li class="main-nav__item">
-            <a href="/study" class="nav-link">Study</a>
+            <a href="/study">STUDY</a>
           </li>
           <li class="main-nav__item">
-            <a href="/" class="nav-link">Community</a>
+            <a href="/">COMMUNITY</a>
           </li>
           <li class="main-nav__item">
-            <a href="/" class="nav-link">Sign in</a>
+            <a href="/">Sign in</a>
           </li>
-          <li class="main-nav__item">
-            <a href="/" class="nav-link">Sign up</a>
+          <li class="main-nav__item--cta">
+            <a href="/">Sign up</a>
           </li>
         </ul>
       </nav>
@@ -30,12 +30,25 @@
       <!-- NuxtPage들 나오는 곳임! -->
       <slot></slot>
     </div>
+    <!-- start: footer-->
+    <footer>
+      <ul>
+        <li>COPi</li>
+        <li><a href="/">COP<span>i</span>소개</a></li>
+        <li><a href="/">이용약관</a></li>
+        <li><a href="/">개인정보처리방침</a></li>
+        <li><a href="/">광고상품소개</a></li>
+      </ul>
+      <div class="copyright">©COPI. ALL RIGHTS RESERVED</div>
+      <a href="#top" class="go-top">위로</a>
+    </footer>
+    <!-- end: footer-->
   </div>
 </template>
 
 <script setup lang="ts">
 const pageContainerStyle = computed(() => ({
-  maxWidth: '1200px',
+  maxWidth: '1080px',
   margin: '0 auto',
 }));
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: [
-    '@/assets/css/common.css',
-    '@/assets/css/include.css',
+    '@/assets/css/common.css', // reset
+    '@/assets/css/main.css', // 글꼴
+    '@/assets/css/include.css', // header, footer
     '@/assets/css/button.css',
   ],
   typescript: {
