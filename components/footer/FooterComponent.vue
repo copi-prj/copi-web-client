90d6143c4341c834acbf36d5da5a6ce12414e4f0
330sum <330sum@naver.com>
2024-08-03 20:34:49 +0900

    Feat(.welcome): header/footer 컴포넌트화

---
+++ components/footer/FooterComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrap">
+    <footer>
+      <ul>
+        <li>COPi</li>
+        <li><a href="/">COP<span>i</span>소개</a></li>
+        <li><a href="/">이용약관</a></li>
+        <li><a href="/">개인정보처리방침</a></li>
+        <li><a href="/sum">광고상품소개</a></li>
+      </ul>
+      <div class="copyright">©COPI. ALL RIGHTS RESERVED</div>
+      <a id="goTopBtn" href="#top" class="go-top"><i class="fa-regular fa-circle-up"></i></a>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+/* 전체 배경 */
+/* .wrap {
+  background: #1b1b1b;
+} */
+
+header,
+footer {
+  position: relative;
+  z-index: 1;
+}
+</style>
